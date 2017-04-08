d233a7b2425f05c1d1b6b7c2a525ce590201e7e0
donaldrshade <donaldrshade@cedarville.edu>
2017-04-08 14:18:53 -0400

    Parsing has begun! I began the readCircuitDes function. I can read the keywords and I should be able to build from there. This should also bleed over into the next function. Should be ready to build gates soon.

--- circuit.cpp
+++ circuit.cpp
@@ -10,3 +10,63 @@ Date Started: April 6, 2017
 */
 
 #include "circuit.h"
+#include <fstream>
+#include <iostream>
+
+
+
+void Circuit::readCircuitDescription(string f){
+	string filename = f + ".txt";
+	ifstream inputFile;
+	inputFile.open(filename);
+	string input;
+	while ( getline(inputFile, input)) {
+		string keyword = input.substr(0, input.find(" "));
+		if (keyword == "CIRCUIT") {
+			setName(input.substr(input.find(" "), input.length()));
+		}
+		else if (keyword == "INPUT") {
+			//create an input
+		}
+		else if (keyword == "OUTPUT") {
+			//create an output
+		}
+		else if (keyword == "AND") {
+			//create a gate
+		}
+		else if (keyword == "NAND") {
+			//create a gate
+		}
+		else if (keyword == "OR") {
+			//create a gate
+		}
+		else if (keyword == "NOR") {
+			//create a gate
+		}
+		else if (keyword == "XOR") {
+			//create a gate
+		}
+		else if (keyword == "XNOR") {
+			//create a gate
+		}
+		else if (keyword == "NOT") {
+			//create a gate
+		}
+	}
+}
+
+void Circuit::readVectorFile(string f){
+	string filename = f + "_v.txt";
+}
+
+void Circuit::simulate(){
+
+}
+
+void Circuit::outputTraces(){
+
+}
+
+void Circuit::setName(string n){
+	name = n;
+}
