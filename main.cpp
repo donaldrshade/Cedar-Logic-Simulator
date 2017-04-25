c338fd6f46f50179972b8046954dccf2cb9bc1bb
donald <donaldrshade@cedarville.edu>
2017-04-25 08:26:26 -0700

    Changed the main file in order to make testing multiple files at once easier.

--- main.cpp
+++ main.cpp
@@ -12,9 +12,9 @@ Date Started: April 6, 2017
 #include <stdio.h>
 #include "circuit.h"
 
-int main(){
+int main() {
 	string filename;
-	bool testing = true;
+	bool testing = false;
 	if (testing) {
 		filename = "circuit0";
 	}
@@ -22,16 +22,22 @@ int main(){
 		cout << "What file would you like to read from?(Filename only. No .txt or _v.txt.) ";
 		cin >> filename;
 	}
-	
-    Circuit c;
-    //1. parse the input file
-    c.readCircuitDescription(filename);
-    //2. parse the vector file
-    c.readVectorFile(filename);
-    //3. simulate the circuit
-    c.simulate();
-    //4. output the traces
-    c.outputTraces();
-	string wait;
-	cin >> wait;
+	while (filename != "exit") {
+		Circuit c;
+		//1. parse the input file
+		c.readCircuitDescription(filename);
+		//2. parse the vector file
+		c.readVectorFile(filename);
+		//3. simulate the circuit
+		c.simulate();
+		//4. output the traces
+		c.outputTraces();
+		if (testing) {
+			filename = "circuit0";
+		}
+		else {
+			cout << "What file would you like to read from?(Filename only. No .txt or _v.txt.) ";
+			cin >> filename;
+		}
+	}
 }
